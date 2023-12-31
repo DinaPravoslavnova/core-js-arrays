faa87e2b3ef18dadc0fb92917ab76d0e0f9002ce
Dina P <dinchik@bk.ru>
2023-12-31 23:33:31 +0400

    feat: add solution for the 9th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -264,8 +264,10 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const newSet = new Set(arr);
+  const uniqueArray = [...newSet];
+  return uniqueArray;
 }
 
 /**
