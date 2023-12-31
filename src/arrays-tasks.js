cef8444590d45ac6f680d858ab3894ef1f41b4ec
Dina P <dinchik@bk.ru>
2024-01-01 03:08:06 +0400

    feat: add solution for the 15th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -41,8 +41,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLengthArray = Math.max(arr1.length, arr2.length);
+  const resultArray = Array.from(
+    { length: maxLengthArray },
+    (element, index) => (arr1[index] || 0) + (arr2[index] || 0)
+  );
+  return resultArray;
 }
 
 /**
