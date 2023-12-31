b6af32181d67f4b61af0e132616e21ce8822160b
Dina P <dinchik@bk.ru>
2023-12-31 22:25:37 +0400

    feat: add solution for the 7th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -211,8 +211,12 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  let resultTailArray = [];
+  if (n > 0) {
+    resultTailArray = arr.slice(-n);
+  }
+  return resultTailArray;
 }
 
 /**
