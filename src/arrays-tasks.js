f68a718f5d06857c24bcfabe575dab2891577893
Dina P <dinchik@bk.ru>
2024-01-01 02:12:28 +0400

    feat: add solution for the 13th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -369,8 +369,13 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) return [];
+
+  const newArray = new Array(len).fill(1);
+  const oddNumbersArray = newArray.map((element, index) => 2 * index + element);
+
+  return oddNumbersArray;
 }
 
 /**
