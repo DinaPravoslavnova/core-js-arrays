1da9479431005fa9c3dfbf9c83184cda5ecb0778
Dina P <dinchik@bk.ru>
2023-12-31 23:46:07 +0400

    feat: add solution for the 10th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -315,8 +315,9 @@ function flattenArray(/* nestedArray */) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  const resultArray = arr.flatMap(childrenSelector);
+  return resultArray;
 }
 
 /**
