5adc6c25406481c133b7095c7694b5bd419c2348
Dina P <dinchik@bk.ru>
2024-01-01 03:16:09 +0400

    feat: add solution for the 16th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -416,8 +416,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const resultFalsyValues = arr.filter((element) => !element).length;
+  return resultFalsyValues;
 }
 
 /**
