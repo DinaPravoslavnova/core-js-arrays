9088bbe064fe9e5811ad015ccd25527e52c0913c
Dina P <dinchik@bk.ru>
2024-01-01 23:44:02 +0400

    feat: add solution for the 31th task arrays-tasks block

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -378,8 +378,14 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const numberOfChunks = Math.ceil(arr.length / chunkSize);
+  return Array.from({ length: numberOfChunks }, (_, index) => {
+    const start = index * chunkSize;
+    const end = (index + 1) * chunkSize;
+    const resultArray = arr.slice(start, end);
+    return resultArray;
+  });
 }
 
 /**
